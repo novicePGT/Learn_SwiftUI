92b21b5bd5a94e4bb70362a4ad3ea5ae40165952
novicePGT <dymnam@naver.com>
2023-08-07 20:49:42 +0900

    [ToggleBasic]

---
+++ Learn_SwiftUI/ToggleBasic.swift
@@ -0,0 +1,35 @@
+//
+//  ToggleBasic.swift
+//  Learn_SwiftUI
+//
+//  Created by 박근태 on 2023/08/07.
+//
+
+import SwiftUI
+
+struct ToggleBasic: View {
+    
+    // property
+    @State var toggleIsOn: Bool = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("로그인 상태: ")
+                Text(toggleIsOn ? "온라인" : "오프라인")
+            } // HStack
+            Toggle(isOn: $toggleIsOn) {
+                Text("로그인 상태 선택")
+            }
+            .toggleStyle(SwitchToggleStyle(tint: Color.red))
+            Spacer()
+        } // VStack
+        .padding(.horizontal, 100)
+    }
+}
+
+struct ToggleBasic_Previews: PreviewProvider {
+    static var previews: some View {
+        ToggleBasic()
+    }
+}
